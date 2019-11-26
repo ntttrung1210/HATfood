ae60409ef834f72cc9d3457b46ac27eccf89e78f
ntttrung1210 <56528546+ntttrung1210@users.noreply.github.com>
2019-11-26 20:32:25 +0700

    done

--- hatweb.py
+++ hatweb.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request, redirect, url_for, session
+from hfdb import*
 app = Flask(__name__)
 
 
 @app.route('/')
 def main_page():
-    return render_template('index.html')
+    return render_template('index.html',data=get_all_food())
+@app.route('/templates/abouthat.html')
+def bmi():
+    return render_template('abouthat.html')
+# @app.route('/templates/abouthat.html',methods=['POST'])
+# # def bmi1():
+# #   return render_template('abouthat.html')
 
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8000, debug=True)
